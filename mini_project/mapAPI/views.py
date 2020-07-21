a541666e41f61df127846f06181739a8ce47ad5e
heej93 <68307130+heej93@users.noreply.github.com>
2020-07-21 18:00:12 +0900

    맵 위치 수정

--- mini_project/mapAPI/views.py
+++ mini_project/mapAPI/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
+# from shop.models import Product
+# from .forms import AddProductForm
+# from .cart import Cart
+
 
 def map_test(request):
     
     return render(request, 'mapAPI/map_test.html')
 
 def main_test(request):
-
-    
+   
     return render(request, 'mapAPI/main_v2.0.html')
 
