e064cacaedc65474ec4b12ebdc38496f8bb2fbed
inhwan9573 <59638485+inhwan9573@users.noreply.github.com>
2020-07-22 17:40:01 +0900

    mapAPI 직접 끌어넣기

---
+++ mini_project/mapAPI/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.map_test),
+    path('main/',views.main_test),
+    path('filter/',views.filter),
+]
--- mini_project/mapAPI/views.py
+++ mini_project/mapAPI/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 def map_test(request):
     
     return render(request, 'mapAPI/map_test.html')
 
 def main_test(request):
+   
+    return render(request, 'mapAPI/main_v2.0.html')
 
-    
-    return render(request, 'mapAPI/main_v0.3.html')
+def filter(request):
+   
+    return render(request, 'mapAPI/filter.html')
+
 
