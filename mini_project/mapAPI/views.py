498c07c2507c314fddfb35027325e4f7a20f8348
heej93 <68307130+heej93@users.noreply.github.com>
2020-07-23 17:41:37 +0900

    디자인완성

--- mini_project/mapAPI/views.py
+++ mini_project/mapAPI/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def main_test(request):
 
-    return render(request, 'mapAPI/main_v2.0.html')
+    return render(request, 'mapAPI/main_v2.5.html')
 
 
 def filter(request):
