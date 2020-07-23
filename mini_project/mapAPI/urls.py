89ca75907f7dc149612ab8ee5b9838136d907c56
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-07-23 15:44:33 +0900

    button disabled if (!text)

--- mini_project/mapAPI/urls.py
+++ mini_project/mapAPI/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.map_test),
     path('main/',views.main_test),
     path('filter/',views.filter),
-    path('link_to_Naver/',views.link_to_NaverMap, name="naver"),
 ]
