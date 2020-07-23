5dde0ba592a0e0556665239a07df56d5096f6f75
heej93 <68307130+heej93@users.noreply.github.com>
2020-07-23 21:39:34 +0900

    ㅍㅍㅍㅍ

--- mini_project/mapAPI/views.py
+++ mini_project/mapAPI/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def main_test(request):
 
-    return render(request, 'mapAPI/main_v2.5.html')
+    return render(request, 'mapAPI/main_v2.0.html')
 
 
 # def path_search(request):
