4ddf154526fddc1cf5ab361c76e4e3fd0a9a1533
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-07-23 14:11:09 +0900

    add click_list's stack limit

--- mini_project/mapAPI/views.py
+++ mini_project/mapAPI/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+
 def map_test(request):
-    
+
     return render(request, 'mapAPI/map_test.html')
 
+
 def main_test(request):
-   
+
     return render(request, 'mapAPI/main_v2.0.html')
 
+
 def filter(request):
-   
+
     return render(request, 'mapAPI/filter.html')
 
 
+def link_to_NaverMap(request):
+    print("살려줘")
