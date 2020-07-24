a0d9f3dc89fc4cd604d40901d7c71d77c8c047eb
inhwan9573 <59638485+inhwan9573@users.noreply.github.com>
2020-07-24 09:37:16 +0900

    금요일 9:37 인환

--- mini_project/sign/views.py
+++ mini_project/sign/views.py
@@ -64,8 +64,6 @@ def id_check(request):
 
 # 비밀번호 확인
 def pw_check(request):
-    print(request.GET.get('join_pw'))
-    print(request.GET.get('join_pw2'))
     user_pw = request.GET.get('join_pw')
     user_pw2 = request.GET.get('join_pw2')
     if user_pw == user_pw2:
@@ -76,7 +74,6 @@ def pw_check(request):
 
 # 회원정보 확인
 def user_info(request):
-    print(request.session.get('user_id'))
     user_id = request.session.get('user_id')
     user = User.objects.get(user_id=user_id)
     return JsonResponse(model_to_dict(user), safe=False)
